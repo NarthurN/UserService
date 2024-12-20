e9a0c9b8b6f5017a5b50b94397584e7746e59ade
Артур Низамитдинов <anezamus-10@mail.ru>
2024-12-20 16:24:39 +0300

    Add server with POST /register

---
+++ server/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"crypto/md5"
+	"io"
+	"net/http"
+	"strings"
+)
+
+type User struct {
+	Login    string
+	Password string
+	Name     string
+}
+
+var users = map[string]User{}
+
+
+func main() {
+	http.HandleFunc("/register", handleRegister)
+	
+	if err := http.ListenAndServe(":9000", nil); err != nil {
+		panic(err)
+	}
+}
+
+func handleRegister(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Метод не поддерживается", http.StatusMethodNotAllowed)
+		return
+	}
+
+	bodyBytes, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Не удалось прочитать запрос", http.StatusBadRequest)
+		return
+	}
+	
+	data := strings.Split(string(bodyBytes), " ")
+	if len(data) != 3 {
+		http.Error(w, "Нужен текст в виде: login password name", http.StatusBadRequest)
+		return
+	}
+	login, password, name := data[0], data[1], data[2]
+	if login == "" || password == "" || name == "" {
+		http.Error(w, "login password name не должны быть пустыми", http.StatusBadRequest)
+		return
+	}
+
+	token := fmt.Sprintf("%s", md5.Sum([]byte(login))) // получаем 265dsfsd54vd3434
+	if _, ok := users[token]; !ok {
+		http.Error(w, "Такой пользователь уже существует", http.StatusForbidden)
+		return
+	} 
+	users[token] = User{
+		Login: login,
+		Password: password,
+		Name: name,
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	_, _ = fmt.Fprint(w, "Пользователь успешно создан")
+}
